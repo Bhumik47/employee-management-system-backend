55e5b802a18a24e241221d9d0da92dee89a57748
Bhumik47 <bhumikdehane@gmail.com>
2024-01-13 00:21:07 +0530

    fixed import statement

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const userRouter = require("./routes/user.js");
-const departmentsRouter = require("")
+const departmentsRouter = require("./routes/department.rouets.js")
 
 const { config } = require("dotenv");
 const cookieParser = require("cookie-parser");
