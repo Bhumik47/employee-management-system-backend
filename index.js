f89aa9967f0766309d4d09956a1788952f632b16
Bhumik47 <bhumikdehane@gmail.com>
2024-04-03 21:03:36 -0700

    origin change

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
   cors({
     origin:
-      /* "http://localhost:5173" */ "https://employee-management-system-front.vercel.app/",
+      "http://localhost:5173" /* "https://employee-management-system-front.vercel.app/" */,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
